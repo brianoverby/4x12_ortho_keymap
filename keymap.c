00d2f4897637383bf56e3c8fcd9514246ab8e377
Brian Overby <brian@MiniMax.local>
2021-10-08 15:44:33 +0200

    Key Override and EEPROM saves

--- keymap.c
+++ keymap.c
@@ -1,61 +1,62 @@
 #include QMK_KEYBOARD_H
 #include "keymap_danish.h"
 
+// Define keymap layers
 enum keymap_layers {
   _BASE,
-  _GAME,
   _LOWER,
   _RAISE,
   _FN,
   _SYSTEM
 };
 
+// Define custom keycodes
 enum keymap_custom_keycodes {
-  COMM = SAFE_RANGE,     // , | <
-  DOT,                   // . | >
-  SLSH,                  // / | ?
-  SCLN,                  // ; | :
-  QUOT,                  // ' | "
-  WNTG,                  // WinKey toggle keycode
-  MACTG,                 // MacOS toggle keycode
+  WIN_TOG = SAFE_RANGE,  // WinKey toggle keycode
+  MAC_TOG,               // Toggle Mac layout
   NEW_SAFE_RANGE
 };
 
-/* Define mod keys */
-#define GAME TG(_GAME)
+// Define mod keys
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define FN MO(_FN)
-#define RSET RESET
 
 
-// For Win key and game mode toggle
+// User config 
+typedef union {
+  uint32_t raw;
+  struct {
+    bool mac_layout_enabled :1;
+  };
+} user_config_t;
+user_config_t user_config;
+
+
+// Init user settings
+void keyboard_post_init_user(void) {
+  user_config.raw = eeconfig_read_user(); // Read config from EEPROM
+};
+
+
+// For Win key toggle
 bool winkey_enabled = true;
-bool gamemode_enabled = false;
-bool mac_layout = true;
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_planck_grid(
         KC_TAB,  DK_Q,    DK_W,    DK_E,    DK_R,    DK_T,    DK_Y,    DK_U,    DK_I,    DK_O,    DK_P,    KC_BSPC,
-        KC_ESC,  DK_A,    DK_S,    DK_D,    DK_F,    DK_G,    DK_H,    DK_J,    DK_K,    DK_L,    SCLN,    QUOT,
-        KC_LSFT, DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    DK_N,    DK_M,    COMM,    DOT,     KC_UP,   KC_ENT,
-        FN,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   SLSH,    KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-
-    [_GAME] = LAYOUT_planck_grid(
-        DK_1,    DK_Q,    DK_W,    DK_E,    DK_R,    DK_T,    DK_Y,    DK_U,    DK_I,    DK_O,    DK_P,    KC_BSPC,
-        _______, DK_A,    DK_S,    DK_D,    DK_F,    DK_G,    DK_H,    DK_J,    DK_K,    DK_L,    SCLN,    QUOT,
-        _______, DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    DK_N,    DK_M,    COMM,    DOT,     KC_UP,   KC_ENT,
-        _______, DK_4,    DK_3,    DK_2,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   SLSH,    KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ESC,  DK_A,    DK_S,    DK_D,    DK_F,    DK_G,    DK_H,    DK_J,    DK_K,    DK_L,    DK_SCLN, DK_QUOT,
+        KC_LSFT, DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    DK_N,    DK_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
+        FN,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   DK_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_LOWER] = LAYOUT_planck_grid(
         DK_TILD, DK_EXLM, DK_AT,   DK_HASH, DK_DLR,  DK_PERC, DK_CIRC, DK_AMPR, DK_ASTR, DK_LPRN, DK_RPRN, KC_BSPC,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_UNDS, DK_PLUS, DK_LCBR, DK_RCBR, DK_ACUT,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_BSLS, DK_PIPE, DK_MINS, DK_EQL,  DK_LBRC, DK_RBRC, _______,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_SPC,  KC_SPC,  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
     [_RAISE] = LAYOUT_planck_grid(
@@ -73,109 +74,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SYSTEM] = LAYOUT_planck_grid(
-        XXXXXXX, XXXXXXX, WNTG,    XXXXXXX, RSET,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GAME,    XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MACTG,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, WIN_TOG, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MAC_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
 
 
+// This is a hack to match US keycap legends on a Danish keyboard layout - using Key Override
+const key_override_t shift_comm_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, DK_LABK);
+const key_override_t shift_dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, DK_RABK);
+const key_override_t shift_quot_key_override = ko_make_basic(MOD_MASK_SHIFT, DK_QUOT, DK_DQUO);
+const key_override_t shift_scln_key_override = ko_make_basic(MOD_MASK_SHIFT, DK_SCLN, DK_COLN); 
+const key_override_t shift_slsh_key_override = ko_make_basic(MOD_MASK_SHIFT, DK_SLSH, DK_QUES); 
 
+// Key Override array
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &shift_comm_key_override,
+    &shift_dot_key_override,
+    &shift_quot_key_override,
+    &shift_scln_key_override,
+    &shift_slsh_key_override,
+    NULL
+};
+
+
+// SYSTEM layer activation
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _SYSTEM);
 }
 
-// Send keycodes matching the legend on the keycaps (US keycaps, Danish keyboard layout in OS)
-
+// Process custom keycodes
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
 
-    case COMM:    // , | <
-      if (record->event.pressed){
-        if (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT)){
-          if(mac_layout) {
-            unregister_code16(KC_LSFT);  // Remove shift before sending the keycode
-            register_code16(KC_NUBS);     // This is < on Danish Mac keyboard
-            register_code16(KC_LSFT);
-          } else {
-            unregister_code16(KC_LSFT);  // Remove shift before sending the keycode
-            register_code16(DK_LABK);
-            register_code16(KC_LSFT);
-          }            
-        } else {
-          register_code16(KC_COMM);
-        }
-      } else {
-        unregister_code16(KC_GRV);
-        unregister_code16(DK_LABK);
-        unregister_code16(KC_COMM);
+    case MAC_TOG: // Mac toggle
+      if (record->event.pressed) {
+        user_config.mac_layout_enabled ^= 1; // Toggles the status
+        eeconfig_update_user(user_config.raw); // Writes to EEPROM!
       }
       return false;
       break;
 
-    case DOT:    // . | >
-      if (record->event.pressed){
-        if (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT)){
-          if(mac_layout) {
-            register_code16(KC_NUBS);
-          } else {
-            register_code16(DK_RABK);
-          }
-        } else {
-          register_code16(KC_DOT);
-        }
-      } else {
-        unregister_code16(KC_GRV);
-        unregister_code16(DK_RABK);
-        unregister_code16(KC_DOT);
-      }
-      return false;
-      break;
-
-    case SLSH:    // / | ?
-      if (record->event.pressed){
-        if (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT)){
-          register_code16(DK_QUES);
-        } else {
-          register_code16(DK_SLSH);
-        }
-      } else {
-        unregister_code16(DK_QUES);
-        unregister_code16(DK_SLSH);
-      }
-      return false;
-      break;
-
-    case SCLN:    // ; | :
-      if (record->event.pressed){
-        if (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT)){
-          register_code16(DK_COLN);
-        } else {
-          register_code16(DK_SCLN);
-        }
-      } else {
-        unregister_code16(DK_COLN);
-        unregister_code16(DK_SCLN);
-      }
-      return false;
-      break;
-
-    case QUOT:    // ' | "
-      if (record->event.pressed){
-        if (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT)){
-          register_code16(DK_DQUO);
-        } else {
-          register_code16(DK_QUOT);
-        }
-      } else {
-        unregister_code16(DK_DQUO);
-        unregister_code16(DK_QUOT);
-      }
-      return false;
-      break;
-    
-    case WNTG:
+    case WIN_TOG: // Windows Key toggle
       if (record->event.pressed) { // Toggle Windows Key
         winkey_enabled = !winkey_enabled;
       }
@@ -190,36 +132,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
     
-    case GAME:
-      if (record->event.pressed) { // Toggle game mode
-        gamemode_enabled = !gamemode_enabled;
-      }
-      return false;
-      break;
-    
-    case MACTG:
-      if (record->event.pressed) { // Toggle Windows / Mac layout
-        mac_layout = !mac_layout;
-      }
-      return false;
-      break;
-    
-    case DK_TILD: // Send TAB, when game mode is enabled else send ~ or `
-    case DK_GRV:
-      if(gamemode_enabled) {
-        if (record->event.pressed) {
-          register_code(KC_TAB);
-        } else {
-          unregister_code(KC_TAB);
-        }
-        return false;
-      } else {
-        return true;
-      }
-      break;
-
-    case DK_AT:
-      if(mac_layout) {
+    case DK_AT: // @: PC ALGR(KC_2), Mac LALT(KC_NUHS)
+      if(user_config.mac_layout_enabled) {
         if (record->event.pressed) {
           register_code16(LALT(KC_NUHS));
         } else {
@@ -231,8 +145,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
     
-    case DK_DLR:
-      if(mac_layout) {
+    case DK_DLR: // $: PC ALGR(KC_4), Mac LSFT(LALT(KC_3))
+      if(user_config.mac_layout_enabled) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_3)));
         } else {
@@ -244,8 +158,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
     
-    case DK_BSLS:
-      if(mac_layout) {
+    case DK_BSLS: // (backslash): PC ALGR(KC_NUBS), Mac LSFT(LALT(KC_7))
+      if(user_config.mac_layout_enabled) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_7)));
         } else {
@@ -257,8 +171,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 
-    case DK_PIPE:
-      if(mac_layout) {
+    case DK_PIPE: // |: PC ALGR(KC_EQL), Mac LALT(KC_I)
+      if(user_config.mac_layout_enabled) {
         if (record->event.pressed) {
           register_code16(LALT(DK_I));
         } else {
@@ -270,8 +184,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 
-    case DK_LCBR:
-      if(mac_layout) {
+    case DK_LCBR: // {: PC ALGR(KC_7), Mac LSFT(LALT(KC_8))
+      if(user_config.mac_layout_enabled) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_8)));
         } else {
@@ -283,8 +197,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 
-    case DK_RCBR:
-      if(mac_layout) {
+    case DK_RCBR: // {: PC ALGR(KC_0), Mac LSFT(LALT(KC_9))
+      if(user_config.mac_layout_enabled) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_9)));
         } else {
