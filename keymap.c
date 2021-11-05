d8107bb9ea92ec81455c785e96ed0208c50d4bf0
Brian Overby <GQ47PW@BOVW11.its.aau.dk>
2021-11-05 15:08:14 +0100

    Moved F-keys

--- keymap.c
+++ keymap.c
@@ -48,29 +48,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_4x12(
         KC_TAB,  DK_Q,    DK_W,    DK_E,    DK_R,    DK_T,    DK_Y,    DK_U,    DK_I,    DK_O,    DK_P,    KC_BSPC,
         KC_ESC,  DK_A,    DK_S,    DK_D,    DK_F,    DK_G,    DK_H,    DK_J,    DK_K,    DK_L,    DK_SCLN, DK_QUOT,
-        KC_LSFT, DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    DK_N,    DK_M,    KC_COMM, KC_DOT,  DK_SLSH, KC_ENT,
-        FN,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_LSFT, DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    DK_N,    DK_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
+        FN,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   DK_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_LOWER] = LAYOUT_ortho_4x12(
         DK_TILD, DK_EXLM, DK_AT,   DK_HASH, DK_DLR,  DK_PERC, DK_CIRC, DK_AMPR, DK_ASTR, DK_LPRN, DK_RPRN, KC_BSPC,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_UNDS, DK_PLUS, DK_LCBR, DK_RCBR, DK_ACUT,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_BSLS, DK_PIPE, DK_MINS, DK_EQL,  DK_LBRC, DK_RBRC, _______,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_SPC,  KC_SPC,  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   DK_UNDS, DK_PLUS, DK_LCBR, DK_RCBR, DK_ACUT,
+        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DK_MINS, DK_EQL,  DK_LBRC, DK_RBRC, _______,
+        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_SPC,  KC_SPC,  _______, DK_BSLS, DK_PIPE, XXXXXXX, XXXXXXX
     ),
 
     [_RAISE] = LAYOUT_ortho_4x12(
         DK_GRV,  DK_1,    DK_2,    DK_3,    DK_4,    DK_5,    DK_6,    DK_7,    DK_8,    DK_9,    DK_0,    KC_BSPC,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_DIAE,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,  KC_MPLY
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_DIAE,
+        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, _______, _______, KC_VOLU, _______,
+        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, XXXXXXX, KC_MNXT, KC_VOLD,  KC_MPLY
     ),
 
     [_FN] = LAYOUT_ortho_4x12(
-        KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, KC_PSCR, DK_ARNG,
-        KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_AE,   DK_OSTR,
-        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+        KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, KC_PSCR, DK_ARNG,
+        KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_AE,   DK_OSTR,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_DEL,
+        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, XXXXXXX, KC_HOME, KC_PGDN, KC_END
     ),
 
     [_SYSTEM] = LAYOUT_ortho_4x12(
