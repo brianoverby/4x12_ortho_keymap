7d0920c654353ced053f8ae298c8a6cc277601e5
Brian Overby <GQ47PW@bovw10.its.aau.dk>
2022-05-05 14:01:12 +0200

    small updates

--- keymap.c
+++ keymap.c
@@ -12,8 +12,7 @@ enum keymap_layers {
 
 // Define custom keycodes
 enum keymap_custom_keycodes {
-  WIN_TOG = SAFE_RANGE,  // WinKey toggle keycode
-  MAC_TOG,               // Toggle Mac layout
+  MAC = SAFE_RANGE,  // Toggle Mac layout
   NEW_SAFE_RANGE
 };
 
@@ -27,7 +26,7 @@ enum keymap_custom_keycodes {
 typedef union {
   uint32_t raw;
   struct {
-    bool mac_layout_enabled :1;
+    bool macos :1;
   };
 } user_config_t;
 user_config_t user_config;
@@ -39,17 +38,13 @@ void keyboard_post_init_user(void) {
 };
 
 
-// For Win key toggle
-bool win_key_enabled = true;
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_ortho_4x12(
         KC_TAB,  DK_Q,    DK_W,    DK_E,    DK_R,    DK_T,    DK_Y,    DK_U,    DK_I,    DK_O,    DK_P,    KC_BSPC,
         KC_ESC,  DK_A,    DK_S,    DK_D,    DK_F,    DK_G,    DK_H,    DK_J,    DK_K,    DK_L,    DK_SCLN, DK_QUOT,
-        KC_LSFT, DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    DK_N,    DK_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
-        FN,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   DK_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT, DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    DK_N,    DK_M,    KC_COMM, KC_DOT,  DK_SLSH, KC_ENT,
+        FN,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
     [_LOWER] = LAYOUT_ortho_4x12(
@@ -62,21 +57,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT_ortho_4x12(
         DK_GRV,  DK_1,    DK_2,    DK_3,    DK_4,    DK_5,    DK_6,    DK_7,    DK_8,    DK_9,    DK_0,    KC_BSPC,
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_DIAE,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, _______, _______, KC_VOLU, _______,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, XXXXXXX, KC_MNXT, KC_VOLD,  KC_MPLY
+        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, _______, _______, XXXXXXX, _______,
+        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,  KC_MPLY
     ),
 
     [_FN] = LAYOUT_ortho_4x12(
         KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, KC_PSCR, DK_ARNG,
         KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_AE,   DK_OSTR,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_DEL,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, XXXXXXX, KC_HOME, KC_PGDN, KC_END
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
+        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
 
     [_SYSTEM] = LAYOUT_ortho_4x12(
-        XXXXXXX, XXXXXXX, WIN_TOG, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MAC_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MAC,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
@@ -109,31 +104,16 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
 
-    case MAC_TOG: // Mac toggle
+    case MAC: // Mac toggle
       if (record->event.pressed) {
-        user_config.mac_layout_enabled ^= 1; // Toggles the status
+        user_config.macos ^= 1; // Toggles the status
         eeconfig_update_user(user_config.raw); // Writes to EEPROM!
       }
       return false;
       break;
-
-    case WIN_TOG: // Windows Key toggle
-      if (record->event.pressed) { // Toggle Windows Key
-        win_key_enabled ^= 1;
-      }
-      return false;
-      break;
-    
-    case KC_LGUI: // Check if Windows Key is disabled 
-      if (!win_key_enabled && !user_config.mac_layout_enabled) {
-        return false;
-      } else {
-        return true;
-      }
-      break;
     
     case DK_AT: // @: PC ALGR(KC_2), Mac LALT(KC_NUHS)
-      if(user_config.mac_layout_enabled) {
+      if(user_config.macos) {
         if (record->event.pressed) {
           register_code16(LALT(KC_NUHS));
         } else {
@@ -146,7 +126,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     
     case DK_DLR: // $: PC ALGR(KC_4), Mac LSFT(LALT(KC_3))
-      if(user_config.mac_layout_enabled) {
+      if(user_config.macos) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_3)));
         } else {
@@ -159,7 +139,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     
     case DK_BSLS: // (backslash): PC ALGR(KC_NUBS), Mac LSFT(LALT(KC_7))
-      if(user_config.mac_layout_enabled) {
+      if(user_config.macos) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_7)));
         } else {
@@ -172,7 +152,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
 
     case DK_PIPE: // |: PC ALGR(KC_EQL), Mac LALT(KC_I)
-      if(user_config.mac_layout_enabled) {
+      if(user_config.macos) {
         if (record->event.pressed) {
           register_code16(LALT(DK_I));
         } else {
@@ -185,7 +165,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
 
     case DK_LCBR: // {: PC ALGR(KC_7), Mac LSFT(LALT(KC_8))
-      if(user_config.mac_layout_enabled) {
+      if(user_config.macos) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_8)));
         } else {
@@ -198,7 +178,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
 
     case DK_RCBR: // {: PC ALGR(KC_0), Mac LSFT(LALT(KC_9))
-      if(user_config.mac_layout_enabled) {
+      if(user_config.macos) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_9)));
         } else {
